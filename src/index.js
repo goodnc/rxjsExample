e4fe4029f842b1bca701b101d4c401064eaf7cf2
JackNiu <goodnc@163.com>
2024-11-26 19:32:52 +0800

    Example 11 take, takeWhile, takeUntil.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { fromEvent, interval } from "rxjs";
-import { switchMap } from "rxjs/operators";
+import { fromEvent, range } from "rxjs";
+import { take, takeWhile, takeUntil } from "rxjs/operators";
+
+// range(1, 10)
+// .pipe(take(5)) // Take only the first 5 numbers
+
+// .pipe(takeWhile((n) => n < 3))
+// .subscribe(console.log);
 
 const button = document.getElementById("btn");
 
-fromEvent(button, "click")
-  .pipe(switchMap((event) => interval(1000))) // 将原本是event事件对象转换为了一个interval的可观察者对象。再次点击的时候会抛弃上一次的事件流，从0开始重新发送。
+// 在document区域里移动鼠标，会源源不断地发出数据流，当点击按钮后，数据流就会停止
+fromEvent(document, "mousemove")
+  .pipe(takeUntil(fromEvent(button, "click")))
   .subscribe(console.log);
