745599b4f09ab2be846715607f43f587ced01378
JackNiu <goodnc@163.com>
2024-11-26 14:16:41 +0800

    Example 2.3 可观察者对象是惰性的，只有被订阅了才可以被执行，而且每订阅一次，就执行一次。

--- src/index.js
+++ src/index.js
@@ -2,30 +2,14 @@
 import { Observable } from "rxjs";
 
 // 创建可观察者对象
-const observable = new Observable((subscriber) => {
-  // 产生数据
-  let index = 0;
-  let timer = setInterval(() => {
-    subscriber.next(index++);
-    if (index === 5) {
-      // subscriber.complete();
-      subscriber.error("出错了");
-    }
-  }, 1000);
+// 可观察者对象是惰性的，只有被订阅了才可以被执行
+const observable = new Observable(() => {
+    console.log('Hello RxJS');
 });
 
-// 创建观察者
-const observer = {
-  next: (value) => {
-    console.log(value);
-  },
-  complete: () => {
-    console.log("终止了");
-  },
-  error: (error) => {
-    console.log(error);
-  }
-}
-
 // 订阅
-observable.subscribe(observer)
+observable.subscribe()
+// 可观察者对象可以被多次订阅，每被订阅一次，就会执行一次
+observable.subscribe()
+observable.subscribe()
+observable.subscribe()
