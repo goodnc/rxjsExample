62f5a370a061f49580765c83c19fa45cc939228d
JackNiu <goodnc@163.com>
2024-11-26 15:31:28 +0800

    Example 4 BehaviorSubject.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import { Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
-// Subject 用于创建一个空的可观察者对象，在订阅后不会立即执行，next方法可以在可观察者对象外部调用。
-const subject = new Subject();
+// BehaviorSubject 拥有Subject的所有功能，但是在创建可观察者对象时可以传入一个默认值，观察者订阅后可以直接拿到默认值。
+// Subject 被订阅后不是被立即执行，而BehaviorSubject 被订阅后会被立即执行。
+const behaviorSubject = new BehaviorSubject('Default Behavior');
 
-subject.subscribe({
-  next: (value) => console.log(value),
-});
+behaviorSubject.subscribe({
+  next: (value) => {
+    console.log(value)
+  }
+})
 
-subject.subscribe({
-  next: (value) => console.log(value),
-});
-
-setTimeout(
-  () => subject.next('Hello Subject')
-, 1000);
+behaviorSubject.next('Hello Behavior');
