9348598d6c87e223769a832323b76c881c3e9a5d
JackNiu <goodnc@163.com>
2024-11-27 00:02:37 +0800

    Example 14.1 通过原生js实现div盒子拖动.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import { of } from "rxjs";
 import { distinctUntilChanged } from "rxjs/operators";
 
-// of：将参数列表作为数据流返回
-of("a", 2, true, [], {}).subscribe((v) => console.log(v));
+const box = document.getElementById("box");
 
-// distinctUntilChanged：检测数据源当前发出的数据流是否和上次发出的相同，相同则跳过，不同则发出。
-of(1, 1, 2, 3, 3, 3, 4, 4, 5, 6)
-  .pipe(distinctUntilChanged())
-  .subscribe((v) => console.log(v));
+box.onmousedown = (event) => {
+  let distanceX = event.clientX - event.target.offsetLeft;
+  let distanceY = event.clientY - event.target.offsetTop;
+  document.onmousemove = (event) => {
+    let left = event.clientX - distanceX;
+    let top = event.clientY - distanceY;
+    box.style.left = left + "px";
+    box.style.top = top + "px";
+  };
+  box.onmouseup = () => {
+    document.onmousemove = null;
+  };
+};
